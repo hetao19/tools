d3412141ac19e0546c84ea1863d9fd52b8ef4635
zhaodongke <zhaodongke@zhenrongbao.com>
2019-09-12 17:07:39 +0800

    feat: 添加check_ip

---
+++ pylib/check_ip.py
@@ -0,0 +1,7 @@
+def check_ip(ip):
+    p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
+    if p.match(ip):
+        return True
+    else:
+        return False
+————————————————
